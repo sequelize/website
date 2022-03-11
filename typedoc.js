0b3aa842019ad674a024e7bb8635ad1c3b6570f8
ephys <zoe@ephys.dev>
2022-03-11 23:07:54 +0100

    build: remove typedoc
    typedoc remains in the main repo

--- typedoc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  includeVersion: true,
-  tsconfig: './tsconfig-typedoc.json',
-  entryPoints: ['./.typedoc/package/types/index.d.ts'],
-  out: './static/api/v7',
-  readme: 'none',
-};
