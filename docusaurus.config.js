f5a541d1e5c50a5ac748406aad9b655ab1fda7d5
Sascha Depold <sascha@depold.com>
2022-03-18 09:25:22 +0100

    Improve hero

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,14 +7,13 @@ const lightCodeTheme = require('prism-react-renderer/themes/github');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Sequelize',
-  tagline: 'Promise-based ORM',
+  tagline: 'Sequelize is a promise-based Node.js ORM for Postgres, MySQL, MariaDB, SQLite and Microsoft SQL Server. It features solid transaction support, relations, eager and lazy loading, read replication and more.',
   url: 'https://sequelize.org',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   organizationName: 'sequelize',
   projectName: 'sequelize',
-
   presets: [
     [
       'classic',
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,50 @@
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import clsx from 'clsx';
-import React from 'react';
-import HomepageFeatures from '../components/homepage-features';
-import styles from './index.module.css';
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import clsx from "clsx";
+import React from "react";
+import HomepageFeatures from "../components/homepage-features";
+import styles from "./index.module.css";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
 
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/v6/intro"
-          >
-            Docusaurus Tutorial - 5min
-          </Link>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <div className={clsx("container", styles.heroBannerContainer)}>
+        <img src="/img/logo.svg" className="container-logo" />
+        <div className={styles.textContainer}>
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            <Link
+              className="button button--primary button--lg"
+              to="/docs/v6/getting-started"
+            >
+              Getting Started
+            </Link>
+
+            <Link
+              className="button button--secondary button--lg"
+              to="/docs/v6/getting-started"
+            >
+              API Reference
+            </Link>
+
+            <Link
+              className="button button--secondary button--lg"
+              to="/docs/v6/other-topics/upgrade-to-v6"
+            >
+              Upgrade to V6
+            </Link>
+
+            <Link
+              className={clsx("button button--secondary button--lg", styles.supportButton)}
+              to="https://opencollective.com/sequelize"
+            >
+              Support us
+            </Link>
+          </div>
         </div>
       </div>
     </header>
