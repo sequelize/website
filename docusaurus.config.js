bc2e1833d5d5be8e60e71789b308df0faadbb9b8
Sascha Depold <sdepold@users.noreply.github.com>
2022-03-30 21:13:28 +0200

    Add netlify reference to qualify for open source plan (#41)
    Co-authored-by: Sascha Depold <sdepold@ebay.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,8 @@ const lightCodeTheme = require('prism-react-renderer/themes/github');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Sequelize',
-  tagline: 'Sequelize is a promise-based Node.js ORM for Postgres, MySQL, MariaDB, SQLite and Microsoft SQL Server. It features solid transaction support, relations, eager and lazy loading, read replication and more.',
+  tagline:
+    'Sequelize is a promise-based Node.js ORM for Postgres, MySQL, MariaDB, SQLite and Microsoft SQL Server. It features solid transaction support, relations, eager and lazy loading, read replication and more.',
   url: 'https://sequelize.org',
   baseUrl: '/',
   onBrokenLinks: 'throw',
@@ -49,7 +50,7 @@ const config = {
   ],
 
   themeConfig:
-  /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     {
       navbar: {
         title: 'Sequelize',
@@ -190,7 +191,10 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} Sequelize Contributors. Built with Docusaurus.`,
+        copyright: `
+          Copyright © ${new Date().getFullYear()} Sequelize Contributors. <br/>
+          Built with Docusaurus and <a href="https://www.netlify.com">powered by Netlify</a>.
+        `,
       },
       prism: {
         theme: lightCodeTheme,
