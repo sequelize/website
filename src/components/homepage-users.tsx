6f8c301ddb218b5b08112a456f6cfb5783935c55
Sascha Depold <sascha@depold.com>
2022-04-15 22:49:50 +0200

    Fix linting

--- src/components/homepage-users.tsx
+++ src/components/homepage-users.tsx
@@ -1,42 +1,42 @@
-import clsx from "clsx";
-import React, { useEffect, useState } from "react";
-import { Carousel } from "react-responsive-carousel";
-import styles from "./homepage-users.module.scss";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import clsx from 'clsx';
+import React, { useEffect, useState } from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import styles from './homepage-users.module.scss';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 type User = {
-  image: string;
-  name: string;
-  testimonial: string;
-  style?: any;
-  author?: string;
+  image: string,
+  name: string,
+  testimonial: string,
+  style?: any,
+  author?: string,
 };
 const users: User[] = [
   {
-    image: "/img/bitovi-logo.png",
+    image: '/img/bitovi-logo.png',
     style: { maxHeight: 50 },
-    name: "Bitovi",
+    name: 'Bitovi',
     testimonial:
-      "We have used Sequelize in enterprise projects for some of our Fortune 100 and Fortune 500 clients. It is used in deployments that are depended on by hundreds of millions of devices every year.",
+      'We have used Sequelize in enterprise projects for some of our Fortune 100 and Fortune 500 clients. It is used in deployments that are depended on by hundreds of millions of devices every year.',
   },
   {
-    image: "/img/ermeshotels-logo.png",
-    name: "Ermes Hotels",
+    image: '/img/ermeshotels-logo.png',
+    name: 'Ermes Hotels',
     testimonial:
-      "Using Sequelize in production for two different apps with 30k+ daily users by 2 years. I doubt there is something better at this moment in terms of productivity and features.",
+      'Using Sequelize in production for two different apps with 30k+ daily users by 2 years. I doubt there is something better at this moment in terms of productivity and features.',
   },
   {
-    image: "/img/secure-coders.png",
-    name: "Secure Coders",
-    testimonial: "Sequelize provides a reliable and convenient ORM for any SQL dialect which greatly simplifies working with both simple and complex data models. The data migration features are just icing on the cake.",
-    author: "Charley Wooley",
+    image: '/img/secure-coders.png',
+    name: 'Secure Coders',
+    testimonial: 'Sequelize provides a reliable and convenient ORM for any SQL dialect which greatly simplifies working with both simple and complex data models. The data migration features are just icing on the cake.',
+    author: 'Charley Wooley',
   },
   {
-    image: "/img/logo-snaplytics-green.png",
-    name: "Snaplytics",
+    image: '/img/logo-snaplytics-green.png',
+    name: 'Snaplytics',
     testimonial:
-      "We've been using sequelize since we started in the beginning of 2015. We use it for our graphql servers (in connection with graphql-sequelize), and for all our background workers.",
-    style: { paddingTop: "10px" },
+      'We\'ve been using sequelize since we started in the beginning of 2015. We use it for our graphql servers (in connection with graphql-sequelize), and for all our background workers.',
+    style: { paddingTop: '10px' },
   },
   // {
   //   image: "/img/shutterstock.png",
@@ -44,11 +44,11 @@ const users: User[] = [
   //   style: { paddingTop: "10px" },
   // },
   {
-    image: "/img/walmart-labs-logo.png",
-    name: "Walmart Labs",
+    image: '/img/walmart-labs-logo.png',
+    name: 'Walmart Labs',
     testimonial:
-      "... we are avid users of sequelize and have been for the past 18 months. (Feb 2017)",
-    style: { paddingTop: "15px" },
+      '... we are avid users of sequelize and have been for the past 18 months. (Feb 2017)',
+    style: { paddingTop: '15px' },
   },
 ];
 
@@ -60,14 +60,15 @@ export default function HomepageUsers(): JSX.Element {
     setWidth(window.innerWidth);
   }
 
-  function onChange(index: number) {
+  const onChange = (index: number) => {
     setActiveUser(users[index]);
-  }
+  };
 
   useEffect(() => {
-    window.addEventListener("resize", handleWindowSizeChange);
+    window.addEventListener('resize', handleWindowSizeChange);
+
     return () => {
-      window.removeEventListener("resize", handleWindowSizeChange);
+      window.removeEventListener('resize', handleWindowSizeChange);
     };
   }, []);
 
@@ -82,19 +83,19 @@ export default function HomepageUsers(): JSX.Element {
             showArrows={false}
             showStatus={false}
             showIndicators={false}
-            infiniteLoop={true}
+            infiniteLoop
             showThumbs={false}
-            useKeyboardArrows={true}
-            autoPlay={true}
-            stopOnHover={true}
-            swipeable={true}
-            emulateTouch={true}
+            useKeyboardArrows
+            autoPlay
+            stopOnHover
+            swipeable
+            emulateTouch
             centerSlidePercentage={isMobile ? 75 : 25}
-            centerMode={true}
+            centerMode
             onChange={onChange}
             transitionTime={1000}
           >
-            {users.map((user) => (
+            {users.map(user => (
               <div key={user.name} className={styles.slide}>
                 <img src={user.image} style={user.style} alt={user.name} />
               </div>
